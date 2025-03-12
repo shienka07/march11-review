4d89f1c102b3f27cc4f9de9d42341e7f4f33e4ed
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-03-12 10:12:23 +0900

    merge

--- src/main/java/org/example/pilates_helper/model/repository/TogetherRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.pilates_helper.model.repository;
-
-import org.example.pilates_helper.util.APIClient;
-
-public class TogetherRepository implements APIClient {
-
-}
--- src/main/java/org/example/pilates_helper/service/TogetherService.java
+++
@@ -1,5 +0,0 @@
-package org.example.pilates_helper.service;
-
-public class TogetherService {
-
-}
