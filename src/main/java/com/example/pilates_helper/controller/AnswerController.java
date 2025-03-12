c3d44f0fad2ad2ed4d72c71d04d484ff5de87918
JO KYEONGHYE <shienka07.ko@gmail.com>
2025-03-12 12:22:37 +0900

    메인 페이지 디자인, 질문 페이지 생성

---
+++ src/main/java/com/example/pilates_helper/controller/AnswerController.java
@@ -0,0 +1,24 @@
+package com.example.pilates_helper.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class AnswerController extends Controller{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html");
+        view(req, resp, "index");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String question = req.getParameter("question");
+    }
+}
